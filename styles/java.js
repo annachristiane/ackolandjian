0237d8f2d19238008354a26aff1d3dbe9015d0bb
shadirizkallah <shady.rizkallah@hotmail.com>
2020-05-24 15:34:22 +0200

    sixth commit

--- styles/java.js
+++ styles/java.js
@@ -1,7 +1,7 @@
-function myFunction() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+  function myFunction1() {
+  var dots = document.getElementById("dots1");
+  var moreText = document.getElementById("more1");
+  var btnText = document.getElementById("myBtn1");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
